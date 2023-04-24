8b3596a90a21c3cc6643cdd710318db020d3babf
stexine <stexine@gmail.com>
2023-04-25 02:32:32 +0800

    modify windows keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- Resize window using <ctrl+shift> arrow keys
+map("n", "<C-A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
